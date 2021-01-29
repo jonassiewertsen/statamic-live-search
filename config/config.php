7b7b4f9ad030df2e8452ed6f4a056dcb9397f002
Jonas Siewertsen <jonas.siewertsen@visuellverstehen.de>
2021-01-29 17:38:29 +0100

    Add a proper description to the config file

--- config/config.php
+++ config/config.php
@@ -1,6 +1,20 @@
 <?php
 
 return [
+
+    /*
+   |--------------------------------------------------------------------------
+   | To augmented collection
+   |--------------------------------------------------------------------------
+   |
+   | This is the place where you can define the fields you want being
+   | returned as an search result.
+   |
+   | Technically we will loop through each result and augment the
+   | defined fields by calling `toAugmentedCollection`
+   |
+   */
+
     'augmentedCollection' => [
         'title',
         'url',
